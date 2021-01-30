0f6cbdfae96c3d7596b33be2123ae5474b445e94
Pius Nyongesa <pinyonge@microsoft.com>
2021-01-30 13:51:09 +0300

    - Refactored resources into a custom component

---
+++ MyComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Azure.Core;
+
+namespace AzureQuickstart
+{
+    class MyComponent : Pulumi.ComponentResource
+    {
+        public MyComponent(string name, ComponentResourceOptions opts)
+            : base("pkg:index:MyComponent", name, opts)
+        {
+            // initialization logic.
+            // Create an Azure Resource Group
+            var resourceGroup = new ResourceGroup("resourceGroup", options: new CustomResourceOptions
+            {
+                Protect = false,
+                Parent = this
+            });
+
+            // signal to the UI that this resource has completed construction
+            this.RegisterOutputs(new Dictionary<string, object>
+            {
+                { "resourceGroupName", resourceGroup.Name }
+            });
+        }
+    }
+}
--- MyStack.cs
+++ MyStack.cs
@@ -11,25 +11,36 @@ class MyStack : Stack
     public MyStack()
     {
         // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup("resourceGroup");
+        var resourceGroup = new ResourceGroup("resourceGroup", options: new CustomResourceOptions
+        {
+            Protect = false
+        });
 
         // Create an Azure Storage Account
         var storageAccount = new Account("storage", new AccountArgs
         {
+            
             ResourceGroupName = resourceGroup.Name,
             AccountReplicationType = "LRS",
             AccountTier = "standard",
             Tags =
             {
-                {
-                    "Environment", "Dev"
-                }
+                { "Environment", "Dev" }
             }
+        }, new CustomResourceOptions
+        {
+            DeleteBeforeReplace = true,
+            Aliases = {new Alias { Name = "my-storage-account"} },
+            CustomTimeouts = new CustomTimeouts { Create = TimeSpan.FromMinutes(30) },
+            DependsOn = { resourceGroup },
+            Parent = resourceGroup
         });
 
         // Export the connection string for the storage account
         this.ConnectionString = storageAccount.PrimaryBlobConnectionString;
+        this.StorageUrn = storageAccount.Urn;
     }
     [Output]
     public Output<string> ConnectionString { get; set; }
+    [Output] public Output<string> StorageUrn { get; set; }
 }
