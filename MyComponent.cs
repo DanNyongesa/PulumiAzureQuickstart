90b2e50e91b4ba90148fe722610e4d3c3e2b0041
Pius Nyongesa <pinyonge@microsoft.com>
2021-01-30 13:52:57 +0300

    - Add storage accountConnectionString to component output

--- MyComponent.cs
+++ MyComponent.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Pulumi;
 using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
 
 namespace AzureQuickstart
 {
@@ -17,10 +19,31 @@ namespace AzureQuickstart
                 Parent = this
             });
 
+            // Create an Azure Storage Account
+            var storageAccount = new Account("storage", new AccountArgs
+            {
+
+                ResourceGroupName = resourceGroup.Name,
+                AccountReplicationType = "LRS",
+                AccountTier = "standard",
+                Tags =
+                {
+                    { "Environment", "Dev" }
+                }
+            }, new CustomResourceOptions
+            {
+                DeleteBeforeReplace = true,
+                Aliases = { new Alias { Name = "my-storage-account" } },
+                CustomTimeouts = new CustomTimeouts { Create = TimeSpan.FromMinutes(30) },
+                DependsOn = { resourceGroup },
+                Parent = this
+            });
+
             // signal to the UI that this resource has completed construction
             this.RegisterOutputs(new Dictionary<string, object>
             {
-                { "resourceGroupName", resourceGroup.Name }
+                { "resourceGroupName", resourceGroup.Name },
+                { "storageAccountConnectionString", storageAccount.PrimaryConnectionString }
             });
         }
     }
